abf1161f20d79f0bf6b637cd0cac0c9c449ce5a1
vic538 <777vvv2015@gmail.com>
2025-09-03 03:22:59 +0300

    Add files via upload

---
+++ src/src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,81 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public String showAllUsers(Principal principal, Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+
+        User currentUser = userService.findUserByName(principal.getName());
+
+        model.addAttribute("userEmail", currentUser.getEmail());
+        model.addAttribute("userRoles", currentUser.getRoles());
+        return "admin-page";
+    }
+
+    @GetMapping(value = "/addNewUser")
+    public String addUser(Principal principal, Model model) {
+        model.addAttribute("user", new User());
+
+        User currentUser = userService.findUserByName(principal.getName());
+
+        model.addAttribute("userEmail", currentUser.getEmail());
+        model.addAttribute("userRoles", currentUser.getRoles());
+        return "add-new-user";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@RequestParam(value = "role", required = false) List<String> rolesStr, @ModelAttribute User user) {
+        userService.saveUserWithRole(user, rolesStr);
+        return "redirect:/admin";
+    }
+
+    @PostMapping(value = "/edit/{id}")
+    public String updateUser(@PathVariable("id") int id,
+                             @RequestParam(value = "role", required = false) List<String> rolesStr,
+                             @ModelAttribute User user) {
+        userService.updateUserWithRoles(id, user, rolesStr);
+        return "redirect:/admin";
+    }
+
+    @PostMapping(value = "/delete/{id}")
+    public String deleteUser(@PathVariable("id") int id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+
+    @GetMapping(value = "/user")
+    public String showUser(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        User currentUser = userService.findUserByName(userDetails.getUsername());
+
+        model.addAttribute("user", currentUser);
+        model.addAttribute("userEmail", currentUser.getEmail());
+        model.addAttribute("userRoles", currentUser.getRoles());
+        return "user-page";
+    }
+
+}
---
+++ src/src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,36 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String showUser(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        User currentUser = userService.findUserByName(userDetails.getUsername());
+
+        model.addAttribute("user", currentUser);
+        model.addAttribute("userEmail", currentUser.getEmail());
+        model.addAttribute("userRoles", currentUser.getRoles());
+        return "user-page";
+    }
+}
